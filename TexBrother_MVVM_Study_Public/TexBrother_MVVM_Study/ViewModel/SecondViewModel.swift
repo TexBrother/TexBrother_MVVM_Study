a25b8e57b8cbdedc8f5c4c807f2fba5f093f9c60
hansolnoh95 <hansolnoh95@naver.com>
2022-02-16 22:51:59 +0900

    :art: second answer

--- TexBrother_MVVM_Study_Public/TexBrother_MVVM_Study/ViewModel/SecondViewModel.swift
+++ TexBrother_MVVM_Study_Public/TexBrother_MVVM_Study/ViewModel/SecondViewModel.swift
@@ -20,34 +20,49 @@ final class SecondViewModel {
     let privacyAgree: Observable<Bool>
     let promotionAgree: Observable<Bool>
   }
+    
   struct Output {
     let registerResult: Observable<RegisterModel>
     let registerEnabled: Observable<Bool>
   }
 }
+
 extension SecondViewModel {
-
   func transform (input: Input) -> Output {
-      let registerModel = Observable.combineLatest(input.emailText,
-                                                   input.passwordText,
-                                                   input.nickNameText,
-                                                   input.privacyAgree,
-                                                   input.promotionAgree)
-        .map {email, password, nickname, privacy, promotion -> RegisterModel in
-          let temp = RegisterModel(email: email,
-                                   passWord: password,
-                                   nickName: nickname,
-                                   privacy: privacy,
-                                   promotion: promotion)
-          return temp
-        }
-      
-      // TODO - validate using regex
-      // HINT - CombineLatest, flatMapLatest
-//
-//      let enabled =
-//      let register =
-//      return .init(registerResult: register, registerEnabled: enabled)
+    
+    let registerModel = Observable.combineLatest(input.emailText,
+                                                 input.passwordText,
+                                                 input.nickNameText,
+                                                 input.privacyAgree,
+                                                 input.promotionAgree)
+      .map{ email, passwd, nickname, privacy, promotion -> RegisterModel in
+        let temp = RegisterModel(email: email,
+                                 passWord: passwd,
+                                 nickName: nickname,
+                                 privacy: privacy,
+                                 promotion: promotion)
+        print(temp)
+        return temp
+      }
+    let enabled = Observable.combineLatest(input.emailText,
+                                           input.passwordText,
+                                           input.nickNameText,
+                                           input.privacyAgree)
+      .map{email, passwd, nickname, privacy -> Bool in
+        return email.validate(with: .email) && passwd.validate(with: .password) && nickname.validate(with: .nickname) && privacy
+      }
+    
+    let emailEnabled = input.emailText.map{$0.validate(with: .email)}
+    let passwordEnabeld = input.passwordText.map{$0.validate(with: .password)}
+    let nickNameEnabeld = input.nickNameText.map{$0.validate(with: .nickname)}
+    let registerEnabled = Observable.combineLatest(emailEnabled, passwordEnabeld, nickNameEnabeld)
+      .map{$0.0 && $0.1 && $0.2}
 
+    let register = input.registerBtnClicked
+      .flatMapLatest{
+        return registerModel
+      }
+      
+    return .init(registerResult: register, registerEnabled: enabled)
   }
 }
